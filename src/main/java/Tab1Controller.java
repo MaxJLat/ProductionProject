e8766df4a7f82e5fbeb158c94d0cf3f445c55b47
MyJuniorLife <maxlatura@gmail.com>
2020-09-03 16:13:24 -0400

    Added event handles for record production button and add product button, inside ProductLine.fxml, Produce.fxml, Tab1Controller.java, Tab2Controller.java.

--- src/main/java/Tab1Controller.java
+++ src/main/java/Tab1Controller.java
@@ -1,2 +1,7 @@
+import javafx.event.ActionEvent;
+
 public class Tab1Controller {
+    public void addProduct(ActionEvent actionEvent) {
+        System.out.println("add product button was pressed.");
+    }
 }
--- src/main/java/Tab2Controller.java
+++ src/main/java/Tab2Controller.java
@@ -1,2 +1,7 @@
+import javafx.event.ActionEvent;
+
 public class Tab2Controller {
+    public void recordProduction(ActionEvent actionEvent) {
+        System.out.println("Record production button was pressed.");
+    }
 }
