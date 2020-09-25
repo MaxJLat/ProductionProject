4b3ce297be0bdb5b095ce919982ef735c18af04c
MyJuniorLife <maxlatura@gmail.com>
2020-09-25 13:32:16 -0400

    Added MoviePlayer.java which extends Product.java and implements MultimediaControl.java.
    Contains fields Screen screen, and MonitorType monitorType.
    
    Override toString() inside Movieplayer.java.
    
    Added ProductionRecord.java containing 4 fields, productionNumber, productID, serialNumber, dateProduced. Has two constructors, one to be used by users, and one to be used by database. Override toString() to print all fields. Added getters and setters.

---
+++ src/main/java/ProductionRecord.java
@@ -0,0 +1,66 @@
+import java.util.Date;
+
+public class ProductionRecord {
+
+  //fields
+  private int productionNumber;
+  private int productID;
+  private String serialNumber;
+  private Date dateProduced;
+
+  //Constructor for when the user makes a new ProductionRecord.
+  public ProductionRecord(int productID) {
+    this.productID = productID;
+    productionNumber = 0;
+    serialNumber = "0";
+    dateProduced = new Date();
+  }
+
+  //Constructor to be used when fetching from Database.
+  public ProductionRecord(int productionNumber, int productID, String serialNumber,
+      Date dateProduced) {
+    this.productionNumber = productionNumber;
+    this.productID = productID;
+    this.serialNumber = serialNumber;
+    this.dateProduced = dateProduced;
+  }
+
+  //toString() override which prints all the fields in ProductionRecord.
+  public String toString() {
+    return "Prod. Num: " + productionNumber + " Product ID: " + productID
+        + " Serial Num: " + serialNumber + " Date: " + dateProduced;
+  }
+
+  //Getters and Setters.
+  public int getProductionNum() {
+    return productionNumber;
+  }
+
+  public void setProductionNum(int productionNumber) {
+    this.productionNumber = productionNumber;
+  }
+
+  public int getProductID() {
+    return productID;
+  }
+
+  public void setProductID(int productID) {
+    this.productID = productID;
+  }
+
+  public String getSerialNum() {
+    return serialNumber;
+  }
+
+  public void setSerialNum(String serialNumber) {
+    this.serialNumber = serialNumber;
+  }
+
+  public Date getProdDate() {
+    return dateProduced;
+  }
+
+  public void setProdDate(Date dateProduced) {
+    this.dateProduced = dateProduced;
+  }
+}
